bbd246e7c230caba9d887f95cbefcc840244ec87
Santiago G. Marín <santiago.marin@final.cl>
2020-07-23 16:08:54 -0400

    Updated docs

--- docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"nonce","url":"globals.html#nonce","classes":"tsd-kind-function"}]};
